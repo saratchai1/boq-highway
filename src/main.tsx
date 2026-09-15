d0925ff8eb1146d17833d9c0c724d4af04441bad
saratchai1 <saratchai@gmail.com>
2026-09-15 20:44:04 +0700

    Load final estimating workspace styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './engineering.css'
+import './complete.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
